4a7e98b40046ad1784c26fcca24b0fb73ae17ee5
jt <jt@deftechops.com>
2026-02-25 15:00:59 -0700

    Add standalone testimonials page

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, 'index.html'),
                 services: resolve(__dirname, 'services.html'),
+                testimonials: resolve(__dirname, 'testimonials.html'),
             },
         },
     },
